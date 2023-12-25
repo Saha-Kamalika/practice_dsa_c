50f6d91a08788fa103bba0c261633bac4369a83f
Kamalika Saha <111375654+Saha-Kamalika@users.noreply.github.com>
2023-12-25 14:37:02 +0530

    Update SpecialPrime.c

--- KPIT Training Qs/SpecialPrime.c
+++ KPIT Training Qs/SpecialPrime.c
@@ -3,7 +3,7 @@ int isPrime(int n){
     for(int i=2;i*i<=n;i++){
         if(n%i==0) return 0;
     }
-    return 1;
+    return 1; 
 }
 int findIndex(int *arr, int len, int n){
     for(int i=0;i<len;i++){
